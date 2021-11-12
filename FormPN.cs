adcfcb570c29e25a9bd1f0d89c039b34954376db
Kien <kienlt.software@gmail.com>
2021-11-12 16:36:46 +0700

    Fixed database name.

--- FormPN.cs
+++ FormPN.cs
@@ -19,7 +19,7 @@ namespace ManagerStoreBuilding
         {
             InitializeComponent();
         }
-        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-HS80QSQ; Initial Catalog = QLVatTu; Integrated Security = SSPI;");
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-7RAMVV0\\SQLEXPRESS; Initial Catalog = QLVatTu; Integrated Security = SSPI;");
 
         private bool IsNumber(string s)
         {
